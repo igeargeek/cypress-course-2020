c933ed4126ff070f7c9b4be651cf8039afdb8903
Arthorn kittinukul <joker.arthorn@gmail.com>
2020-02-16 23:08:24 +0700

    [Edited] vending machine buy

--- vending_machine/cypress/integration/insertAndCanBuyDrink/answer.js
+++ vending_machine/cypress/integration/insertAndCanBuyDrink/answer.js
@@ -3,8 +3,37 @@ context('Insert coin and can buy drink', () => {
     cy.visit('https://vending-machine.zerojame.me/')
   })
 
-  it('VM-01', () => {
+
+  it('User insert 10 baht and 5 baht coins and select Pepsi Max', () => {
     cy.get('#ten_coin_button').click()
+    cy.get('#five_coin_button').click()
+    cy.get('[data-cy=buy_button_Pepsi_Max]').click()
+    cy.get(':nth-child(2) > .swal-button').click()
+    cy.get('#push_item_button').click()
+    cy.contains('Your get Pepsi Max')
+    cy.get('.swal-button').click()
+  })
+
+  it('User insert 10 baht, 5 baht, 2 baht and 1 baht coins and select Pepsi Max', () => {
+    cy.get('#ten_coin_button').click()
+    cy.get('#five_coin_button').click()
+    cy.get('#two_coin_button').click()
+    cy.get('#one_coin_button').click()
+    cy.get('[data-cy=buy_button_Pepsi_Max]').click()
+    cy.get(':nth-child(2) > .swal-button').click()
+    cy.get('#push_item_button').click()
+    cy.contains('Your get Pepsi Max')
+    cy.get('.swal-button').click()
+    cy.get('#change_money_button').click()
+    cy.contains('Your change is 10฿x0, 5฿x0, 2฿x1 and 1฿x1')
+    cy.get('.swal-button').click()
+  })
+
+  it.only('User insert 10 baht and 2 baht coins and select Pepsi Max but cant select it because user dont have enough money', () => {
+    cy.get('#ten_coin_button').click()
+    cy.get('#two_coin_button').click()
+    cy.get('[data-cy=buy_button_Pepsi_Max]').click()
+    cy.get('.mat-simple-snackbar > :nth-child(1)').contains('Your money not enough to buy Pepsi Max.')
   })
 
 })
--- vending_machine/cypress/integration/insertAndCanBuyDrink/insertAndCanBuyDrink.spec.js
+++ vending_machine/cypress/integration/insertAndCanBuyDrink/insertAndCanBuyDrink.spec.js
@@ -3,7 +3,15 @@ context('Insert coin and can buy drink', () => {
       cy.visit('https://vending-machine.zerojame.me/')
     })
   
-    it('VM-01', () => {
+    it('User insert 10 baht and 5 baht coins and select Pepsi Max', () => {
+      //
+    })
+
+    it('User insert 10 baht, 5 baht, 2 baht and 1 baht coins and select Pepsi Max', () => {
+      //
+    })
+
+    it.only('User insert 10 baht and 2 baht coins and select Pepsi Max but cant select it because user dont have enough money', () => {
       //
     })
   
